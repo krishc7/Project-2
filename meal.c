55ada72629938fabd22143c6cc5e79be09a9e473
Tran Nguyen <tranhnguyen@cpp.edu>
2022-03-05 16:15:50 -0800

    meal file and header

---
+++ meal.c
@@ -0,0 +1,90 @@
+#include "meal.h"
+
+enum meals {BREAK_FAST, LUNCH, DINNER};
+
+float meal_eaten(int totalDays, float *excess, int carriedMeal[3]){
+    int mealEaten[3];
+    int allowedMeal = totalDays;
+    int mealCost[] = {9,12,16};
+    enum meals meal;
+
+    float totalCost = 0;
+    float actualCost = 0;
+    float excessMeal = 0;
+
+
+    printf("Enter the total breakfast: ");
+    meal = BREAK_FAST;
+    scanf("%d", &mealEaten[meal]);
+    printf("Enter the total lunch    : ");
+    meal= LUNCH;
+    scanf("%d", &mealEaten[meal]);
+    printf("Enter the total dinner   : ");
+    meal = DINNER;
+    scanf("%d", &mealEaten[meal]);
+
+    for(meal = BREAK_FAST; meal <= DINNER; meal++){
+        actualCost += ( mealEaten[meal] + carriedMeal[meal]) * mealCost[meal];
+        totalCost += allowedMeal * mealCost[meal];
+
+        
+        excessMeal += carriedMeal[meal] * mealCost[meal]; //Testing only
+
+     
+    }
+
+    printf("Actual: %.2f\tTotal:%.2f\tExcess:%.2f\n", actualCost, totalCost, excessMeal); //Testing only
+
+    *excess += actualCost - totalCost;
+    return actualCost;
+
+}
+
+float depart_arrival_time(int totalDays, float *excess){
+    int depart_hour, depart_min,arrival_hour, arrival_min;
+    char time[2];
+    int carriedMeal[3] = {0,0,0};
+    float totalCost = 0;
+
+    enum meals meal;
+
+
+    printf("Enter the time of departure: ");
+    scanf("%d:%d %s",&depart_hour,&depart_min,time);
+    if(depart_hour >= 7  && (strcmp(time,"am") == 0)){
+        meal = BREAK_FAST;
+        carriedMeal[meal]--;
+     
+    }
+    else if( (depart_hour < 6 || depart_hour == 12) && (strcmp(time,"pm") == 0) ){
+        
+        meal = LUNCH;
+        carriedMeal[meal]--;
+      
+
+    }
+    else if (depart_hour >=6  && (strcmp(time,"pm") == 0) && depart_hour != 12 ){
+       meal = DINNER;
+        carriedMeal[meal]--;
+    }
+
+    printf("\nEnter the time of arrival: \n");
+    scanf("%d:%d %s",&arrival_hour,&arrival_min,time);
+
+
+
+
+    //Testign carrying meals
+    for(int i = 0; i < 3; i++){
+        printf("%d",carriedMeal[i]);
+    }
+    printf("\n");
+
+   
+
+    totalCost = meal_eaten(totalDays, &excess, carriedMeal);
+    return totalCost; 
+
+}
+
+
