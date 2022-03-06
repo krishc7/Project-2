57184724f7c7f8e6722ed8bedd0b9d68ca5d7e7b
Arabo Hovsepian <ahovsepian@cpp.edu>
2022-03-05 17:30:06 -0800

    v 0.1

---
+++ AraboH_header.h
@@ -0,0 +1,14 @@
+#ifndef ARABOH_HEADER_H
+
+#define ARABOH_HEADER_H
+
+int totalDays;
+float dollarAirfare, dollarCarRental, privateMileExpense;
+
+int totalNumDaysOnTrip ();
+float amountRoundTripAirfare();
+float amountCarRentals();
+void milesDriven();
+bool dollarMileValidation(float);
+
+#endif
